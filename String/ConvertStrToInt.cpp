7ccccb5a9e7fbe4d40f0080b8d81541f03b7878a
Pratik Mhatre <81980118+Pratikm765@users.noreply.github.com>
2026-06-13 14:43:27 +0530

    Update ConvertStrToInt.cpp
    Adding my stoi implementation

--- String/ConvertStrToInt.cpp
+++ String/ConvertStrToInt.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int my_stoi(const string& s) {
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+
+    // Handle optional sign
+    if (s[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (s[i] == '+') {
+        i++;
+    }
+    // Convert characters to integer
+    while (i < s.length()) {
+        if (s[i] < '0' || s[i] > '9') {
+            throw invalid_argument("Invalid character in input");
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+    return sign * result;
+}
+
 int main() {
    string str = "7";
    int num;
